65ad1a122f95339be86b5fba192fdc5d8d8c9920
inv1ct0 <a.m.yudenkov@yandex.ru>
2019-10-30 22:31:58 +0300

    les_1

---
+++ SpaceWars/android/src/com/inv1ct0/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.inv1ct0;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.inv1ct0.SpaceWars;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SpaceWars(), config);
+	}
+}
